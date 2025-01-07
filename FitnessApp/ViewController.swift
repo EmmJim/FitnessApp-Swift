d2d3d79a5bf098cdc1c3020c22e8c5a6e9a8f917
Emmanuel Jimenez <emmsa.jo@hotmail.com>
2025-01-07 11:22:34 -0600

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by emmanuel.jimenez on 07/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
